11ab47fadb8ea00cc086051d77991c2db591170f
mauro2040 <105966365+mauro2040@users.noreply.github.com>
2023-08-18 15:21:55 -0300

    18.08 15.23
    18.08 15.23

--- js/blacklight.js
+++ js/blacklight.js
@@ -1,5 +1,5 @@
 function color() {
-    var elementsToToggle = document.querySelectorAll(".portfolio, .color, .superior, .nome, .icon, .caixa-principal, " +
+    var elementsToToggle = document.querySelectorAll(".portfolio,.box, .color, .superior, .nome, .icon, .caixa-principal, " +
     ".sobre-mim p, .sobre-mim h2, .estatico, .dinamico li span, " +
     ".sobre-mim, .foto, #menu-icon, .menu, .link-menu, " +
     ".cards-projetos, .cargo, .projetos, .projetos h2, " +
@@ -7,9 +7,15 @@ function color() {
     ".rodape p, #color-light, #color-black, .acordion-text, " +
     "#acordion-text, .cartao1, .proj1, .cartao2, .proj2, " +
     ".cartao2, .proj2, .proj3, .cartao3, .proj4, .cartao4, " +
-    ".proj5, .cartao5");
+    ".proj5, .cartao5,.black_light_btn,.black_light_btn:after,. #menu-list");
 
     elementsToToggle.forEach(function(elem) {
         elem.classList.toggle("dark-mode");
     });
 }
+
+const toggle = document.querySelector (".toggle");
+    const stack = document.querySelector (".stack");
+    toggle.onclick = function(){
+    stack.classList.toggle (`active`);
+    }
--- js/script.js
+++ js/script.js
@@ -21,15 +21,15 @@
         }
     }
     function color() {
-    var elementsToToggle = document.querySelectorAll(".portfolio, .color, .superior, .nome, .icon, .caixa-principal, " +
-    ".sobre-mim p, .sobre-mim h2, .estatico, .dinamico li span, " +
-    ".sobre-mim, .foto, #menu-icon, .menu, .link-menu, " +
-    ".cards-projetos, .cargo, .projetos, .projetos h2, " +
-    ".card, .card p, .car h5, .rodape, .rodape h5, " +
-    ".rodape p, #color-light, #color-black, .acordion-text, " +
-    "#acordion-text, .cartao1, .proj1, .cartao2, .proj2, " +
-    ".cartao2, .proj2, .proj3, .cartao3, .proj4, .cartao4, " +
-    ".proj5, .cartao5");
+        var elementsToToggle = document.querySelectorAll(".portfolio,.box, .color, .superior, .nome, .icon, .caixa-principal, " +
+        ".sobre-mim p, .sobre-mim h2, .estatico, .dinamico li span, " +
+        ".sobre-mim, .foto, #menu-icon, .menu, .link-menu, " +
+        ".cards-projetos, .cargo, .projetos, .projetos h2, " +
+        ".card, .card p, .car h5, .rodape, .rodape h5, " +
+        ".rodape p, #color-light, #color-black, .acordion-text, " +
+        "#acordion-text, .cartao1, .proj1, .cartao2, .proj2, " +
+        ".cartao2, .proj2, .proj3, .cartao3, .proj4, .cartao4, " +
+        ".proj5, .cartao5,.black_light_btn, #menu-list");
 
     elementsToToggle.forEach(function(elem) {
         elem.classList.toggle("dark-mode");
@@ -72,12 +72,6 @@ function exibir6() {
     var txt6 = document.getElementById("cardtxt6");
     txt6.classList.toggle("visible");
 }
-
-const toggle = document.querySelector (".toggle");
-    const stack = document.querySelector (".stack");
-    toggle.onclick = function(){
-    stack.classList.toggle (`active`);
-    }
 
     function html5() {
         var descr = document.getElementById("desc");
